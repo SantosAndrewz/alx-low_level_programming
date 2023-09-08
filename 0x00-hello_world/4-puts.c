d3aa4deac7d201cb7c79e0aa07ba7c93874e81bc
SantosAndrewz <sentosandrewz@gmail.com>
2023-09-08 00:22:55 -0700

    Program1edited

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
  * main - A program that prints without using printf function
- * Return: 0 (program successful)
+ *
+ * return (0) -success
  */
 int main(void)
 {
-puts("\"Priogramming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
