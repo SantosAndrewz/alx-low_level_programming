2757b23d74aac3fceeff7517a42a8f2f306bdb78
SantosAndrewz <sentosandrewz@gmail.com>
2023-09-08 01:03:55 -0700

    Program2

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program to print  using printf and ending in a new line
+ *
+ * Return: 0 if the program is successful
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
