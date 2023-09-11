344bffb311b65aeca91d0ed95a5e9fbcb6a6a401
SantosAndrewz <sentosandrewz@gmail.com>
2023-09-10 21:21:11 -0700

    last digit 5

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,7 +14,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	p = n % 10
+	p = n % 10;
 
 	if (p > 5)
 	{
@@ -28,6 +28,5 @@ int main(void)
 	{
 	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, p);
 	}
-
 	return (0);
 }
