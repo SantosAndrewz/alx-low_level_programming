22972ca97a634f94d80a0815afce7d6ce87f3b23
SantosAndrewz <sentosandrewz@gmail.com>
2023-09-11 18:40:33 -0700

    print exclusion

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * description: a program prints lowercase alphabet except q and e
+ *
+ * Return: 0 for success
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		switch (c)
+		{
+			case 'e':
+			case 'q'
+			break;
+
+			default:
+			putchar(c);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
