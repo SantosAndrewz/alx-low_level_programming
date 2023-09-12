60bd1bfd5e97c699b633c1f5b04a204b212841c9
SantosAndrewz <sentosandrewz@gmail.com>
2023-09-12 15:52:51 -0700

    absolutter

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: the interger for which computation is done.
+ *
+ * Return: the ansolute value of n.
+ */
+int _abs(int)
+{
+	while (n < 0)
+	{
+		n = -n;
+	}
+	return (n);
+}
+
