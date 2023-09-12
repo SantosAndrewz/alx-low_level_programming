9a2514212d4064c78c39c6673bc5654e25b32d05
SantosAndrewz <sentosandrewz@gmail.com>
2023-09-11 19:39:02 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.o>
+/**
+ * main - entry point
+ * description: program prints single numbers of base 10 using putchar
+ *
+ * Return: 0 for success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar('0' + i);
+
+		if (i == 9)
+		{
+			putchar('\n');
+		}
+	}
+	return (0);
+}
