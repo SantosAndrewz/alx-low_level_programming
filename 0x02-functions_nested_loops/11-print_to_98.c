8c589ccd22a0b6467c2cbd7231fd115cedc60a3a
SantosAndrewz <sentosandrewz@gmail.com>
2023-09-13 19:39:46 -0700

    natural numbers10

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -29,14 +29,15 @@ void print_to_98(int n)
 	for (; n >= 98; n--)
 	{
 	if (n ==98)
-	}
+	{
 	printf("%d", n);
 	printf("\n");
 	break;
-	{
-	else
 	}
+	else
+	{
 	printf("%d, ", n);
 	}
-
+	}
+	}
 }
