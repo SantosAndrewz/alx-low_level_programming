01b6777fc2a15667fa5f6718811be9fe6e0c477c
SantosAndrewz <sentosandrewz@gmail.com>
2023-09-13 18:27:25 -0700

    natural numbers5

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,11 +12,11 @@ void print_to_98(int n)
 
 	for (a = n; a <= 98; a++)
 	{
-		printf("%d", a);
+		printf("%d, ", a);
 	}
-	if (a != 98)
+	if (a = 98)
 	{
-		printf(", ");
+		printf("%d", a);
 	}
 	else
 	{
