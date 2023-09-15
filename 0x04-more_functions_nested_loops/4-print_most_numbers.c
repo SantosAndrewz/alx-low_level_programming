c9c3b2eafd4fe167237c6295ed7adea7fb290134
SantosAndrewz <sentosandrewz@gmail.com>
2023-09-14 19:10:57 -0700

    more numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,22 +2,15 @@
 /**
  * print_most_numbers - prints the numbers from 0 to 9 except 2 and 4.
  * _putchar: prints a single character.
- * Return: The numbers from 0 to 9
+ * Return: void
  */
 void print_most_numbers(void)
 {
-	int x = 0;
+	char c;
 
-	for (; x <= 9; x++)
+	for (c = '0'; c <= 9; c++)
 	{
-	if (x == 2 || x == 4)
-	{
-	continue;
-	}
-	else
-	{
-	_putchar(x + '0');
-	}
+	_putchar(c);
 	}
 	_putchar('\n');
 }
