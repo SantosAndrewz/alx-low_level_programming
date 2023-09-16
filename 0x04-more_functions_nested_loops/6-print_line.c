926a395681b0de1acbf1a6e33b1501cb33b03838
SantosAndrewz <sentosandrewz@gmail.com>
2023-09-16 04:17:05 -0700

    Prints a line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_line - draws a straight line in the terminal.
+ * _putchar: prints a single character.
+ *
+ * @n: number of times charcter _ is to be printed
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+	_putchar('\n');
+	return;
+	}
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+	_putchar('_');
+	}
+	_putchar('\n');
+}
