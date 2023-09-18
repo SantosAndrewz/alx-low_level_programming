ebc620450014ca513b82f41592b238957be8fb93
SantosAndrewz <sentosandrewz@gmail.com>
2023-09-18 01:08:25 -0700

    Print fibonacci numbers

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * description: prints the first 98 Fibonacci numbers, starting with 1 and 2.
+ *
+ * Return: 0 for success
+ */
+int main(void)
+{
+unsigned long int i;
+unsigned long int x = 1;
+unsigned long int y = 2;
+unsigned long int temp;
+
+printf("%d, %d", 1, 2);
+for (i = 2; i < 98; i++)
+{
+temp = x + y;
+x = y;
+y = temp;
+printf(", %lu", temp);
+}
+printf("\n");
+return (0);
+}
