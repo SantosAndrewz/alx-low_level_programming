8b38aceffa8a5e344d4cc2268df9e5bd5212611a
SantosAndrewz <sentosandrewz@gmail.com>
2023-09-20 08:10:23 -0700

    Key generator

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -19,9 +19,9 @@ srand(time(NULL));
 for (i = 0; i < 100; i++)
 {
 pswd[i] = rand() % 78;
-sum += pswd[i];
+sum += pswd[i] + '0';
 putchar(pswd[i] + '0');
-if ((2772 - sum) < 78)
+if ((2772 - sum) - '0' < 78)
 {
 n = 2772 - sum;
 sum += n;
