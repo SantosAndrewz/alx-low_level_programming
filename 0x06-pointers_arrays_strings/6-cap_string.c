2a88757577b02992c9bd708d86a4137860b02309
SantosAndrewz <sentosandrewz@gmail.com>
2023-09-21 20:07:32 -0700

    capitapize all

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
  * cap_string - capitalizes all words of the string.
- * @a: the input string,
+ * @str: the input string.
  *
  * Return: a pointer to the modified string.
  */
-char *cap_string(char *a)
+char *cap_string(char *str)
 {
 int i = 0;
 
@@ -13,20 +13,20 @@ while (str[++i])
 {
 while (!(str[i] >= 'a' && str[i] <= 'z'))
 i++;
-if (str[i-1] == ' ' ||
-	str [i -1] == ' ' ||
-	str [i -1] == '\t'||
-	str [i -1] == '\n'||
-	str [i -1] == ';' ||
-	str [i -1] == '?' ||
-	str [i -1] == '.' ||
-	str [i -1] == '{' ||
-	str [i -1] == '}' ||
-	str [i -1] == '(' ||
-	str [i -1] == ')' ||
-	str [i -1] == '"' ||
-	str [i -1] == '!' ||
-	str [i -1] == ',' ||
+if (str[i - 1] == ' ' ||
+	str[i - 1] == ' ' ||
+	str[i - 1] == '\t' ||
+	str[i - 1] == '\n' ||
+	str[i - 1] == ';' ||
+	str[i - 1] == '?' ||
+	str[i - 1] == '.' ||
+	str[i - 1] == '{' ||
+	str[i - 1] == '}' ||
+	str[i - 1] == '(' ||
+	str[i - 1] == ')' ||
+	str[i - 1] == '"' ||
+	str[i - 1] == '!' ||
+	str[i - 1] == ',' ||
 i == 0;
 str[i] -= 32;
 i++;
