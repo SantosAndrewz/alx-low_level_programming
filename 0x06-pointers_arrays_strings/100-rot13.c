e8f074c817890754d6977596137d5e209c0416fc
SantosAndrewz <sentosandrewz@gmail.com>
2023-09-23 07:06:00 -0700

    Rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,8 +9,8 @@ char *rot13(char *str)
 {
 int i = 0;
 int x;
-str1 = {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWQYZ"}
-str2 = {"nopqrstuvwxyzabcdefghijklmABCDEFGHIJKLMNOPQRSTUVWQYZ"}
+char str1 = {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWQYZ"}
+char str2 = {"nopqrstuvwxyzabcdefghijklmABCDEFGHIJKLMNOPQRSTUVWQYZ"}
 
 while (str[i] != '\n')
 {
