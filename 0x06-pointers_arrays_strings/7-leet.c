412757e3f2085e1152e75c679b44615558fd143d
SantosAndrewz <sentosandrewz@gmail.com>
2023-09-22 19:02:19 -0700

    Change numbers to vowels

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * leet - encodes a string into 1337.
+ * @str: input string
+ *
+ * Return: pointer to modified string
+ */
+char *leet(char *str)
+{
+char str1[] = "aeotlAEOTL";
+char str2[] = "4307143071";
+int i = 0;
+int x = 0;
+
+while (str[i] != '\0')
+{
+for (x <= 9; x++)
+{
+if (str[i] == str1[x])
+{
+str[i] = str2[x];
+}
+}
+i++;
+}
+return (str);
+}
