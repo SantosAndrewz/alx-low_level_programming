37455e207e34a74644b5373d327a1f495beb1a4a
SantosAndrewz <sentosandrewz@gmail.com>
2023-09-23 06:35:38 -0700

    Rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * rot13 - encodes a string using rot13
+ * @str: the input string
+ *
+ * Return: the pointer to the modified string.
+ */
+char *rot13(char *str)
+{
+int i = 0;
+int x;
+str1[] = {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWQYZ"}
+str2[] = {"nopqrstuvwxyzabcdefghijklmABCDEFGHIJKLMNOPQRSTUVWQYZ"}
+
+while (str[i] != '\n')
+{
+i++
+}
+for (x = 0; x < 52; x++)
+{
+if (str[i] == str[x])
+{
+str1[x] == str2[x];
+break;
+}
+}
+return (str);
+}
