3333670ca9471174283f6fa6497854b5c421b92a
SantosAndrewz <sentosandrewz@gmail.com>
2023-09-23 07:19:10 -0700

    Rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,20 +9,19 @@ char *rot13(char *str)
 {
 int i = 0;
 int x;
-char *str1 = {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWQYZ"};
-char *str2 = {"nopqrstuvwxyzabcdefghijklmABCDEFGHIJKLMNOPQRSTUVWQYZ"};
+char *str1() = {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWQYZ"};
+char *str2() = {"nopqrstuvwxyzabcdefghijklmABCDEFGHIJKLMNOPQRSTUVWQYZ"};
 
 while (str[i] != '\n')
 {
-i++
-}
 for (x = 0; x < 52; x++)
 {
 if (str[i] == str[x])
 {
 str1[x] == str2[x];
-break;
 }
+}
+i++;
 }
 return (str);
 }
