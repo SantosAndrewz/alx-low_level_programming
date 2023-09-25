4825f052e3ed4c6ab50d457dc7ab7eb827a0b01c
SantosAndrewz <sentosandrewz@gmail.com>
2023-09-25 16:40:58 -0700

    Char locate

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * _strchr - locates a character in a string.
  * @s: string to be searched
@@ -15,7 +16,9 @@ int i;
 for (i = 0; s[i] >= '\0'; i++)
 {
 if (s[i] == c)
+{
 return (s + i);
+}
 }
 return (NULL);
 }
