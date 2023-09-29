f9fdb49f6074c4db9c2e73f847d1077a0ba82a9c
SantosAndrewz <sentosandrewz@gmail.com>
2023-09-28 19:57:11 -0700

    wildcmp two strings

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -8,7 +8,7 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-	if (*s2 == '0' && *(s2 + 1) != '\0')
+	if (*s2 == '*' && *(s2 + 1) != '\0')
 	{
 		return (0);
 	}
@@ -20,7 +20,7 @@ int wildcmp(char *s1, char *s2)
 	{
 		return (wildcmp(s1 + 1, s2 + 1));
 	}
-	if (*s2 == '0')
+	if (*s2 == '*')
 	{
 		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
 	}
