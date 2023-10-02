e0b5fddd63152dddbc527d76e659e637d9cbbd82
SantosAndrewz <sentosandrewz@gmail.com>
2023-10-02 06:26:54 -0700

    First 98 fibronacci numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#define LARGEST 10000000000
 /**
  * main - entry point to the program.
  * description: prints the first 98 Fibonacci numbers, starting with 1 and 2.
@@ -8,17 +9,36 @@
 int main(void)
 {
 unsigned long int i;
-unsigned long int x = 1;
-unsigned long int y = 2;
-unsigned long int temp;
+unsigned long int x1 = 1;
+unsigned long int x2 = 2;
+unsigned long int y1 = 0;
+unsigned long int y2 = 0;
+unsigned long int temp1, temp2;
 
-printf("%u, %u", 1, 2);
+printf("%lu, %lu", x1, x2);
 for (i = 2; i < 98; i++)
 {
-temp = x + y;
-x = y;
-y = temp;
-printf(", %lu", temp);
+	if (x1 + x2 > LARGEST || y2 > 0 || y1 > 0)
+	{
+		temp1 = (x1 + x2) / LARGEST;
+		temp2 = (x1 + x2) % LARGEST;
+		y1 = y2 + temp1;
+		y2 = temp2;
+		x1 = x2;
+		x2 = temp2;
+		printf("%lu, %01lu", y1, y2);
+	}
+	else
+	{
+		temp2 = x1 + x2;
+		x1 = x2;
+		x2 = temp2;
+		printf("%lu", x2);
+	}
+	if (i != 97)
+	{
+		printf(", ");
+	}
 }
 printf("\n");
 return (0);
