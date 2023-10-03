1e559d3391b0185b3bb1cb87e630d467157b073e
SantosAndrewz <sentosandrewz@gmail.com>
2023-10-02 18:28:15 -0700

    Mynameis

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - entry point.
+ * description: A program that prints its name.
+ * @argc: argument count.
+ * @argv: argument vectot.
+ *
+ * Return: 0 for success.
+ */
+ int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
