01d2d7f0797d7eab841203aa11e492e264eb2944
SantosAndrewz <sentosandrewz@gmail.com>
2023-10-10 15:36:20 -0700

    Main.c name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *  main - entry point.
+ *  description: prints the name of the fike it is compiled from.
+ *
+ *  Return: 0 for success.
+ */
+int main(void)
+{
+	printf("%s\n", _FILE_);
+	return (0);
+}
