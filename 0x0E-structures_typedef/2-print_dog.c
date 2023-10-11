58c3a977fb43b7420baa001b3213ecb841274b37
SantosAndrewz <sentosandrewz@gmail.com>
2023-10-10 18:40:21 -0700

    print_dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -21,7 +21,5 @@ void print_dog(struct dog *d)
 	{
 		d->owner = "(nil)";
 	}
-	printf("Name: %s\n", d->name);
-	printf("Age: %f\n", d->name);
-	printf("Owner: %s\n", d->owner);
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
