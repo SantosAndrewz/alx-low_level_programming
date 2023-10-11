da5a954b47819886b71f5dd74e45e0e9f3f5b6de
SantosAndrewz <sentosandrewz@gmail.com>
2023-10-10 18:01:40 -0700

    print_dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,6 +13,6 @@ void print_dog(struct dog *d)
 		return;
 	}
 	printf("Name: %s\n", d->name ? d->name : "(nil)");
-	printf("Age: %f\n", d->name ? d->age : "(nil)");
+	printf("Age: %f\n", d->name ? d->age : -1.0);
 	printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
 }
