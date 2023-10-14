1bfa4d3cf1a2f48734e780ab4eafadfc0eb3bc9a
SantosAndrewz <sentosandrewz@gmail.com>
2023-10-13 19:33:01 -0700

    simple operations@

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "3-calc.h"
+#include <stdlib.h>
+
+/**
+ * main - prints results for operations done.
+ * @argc: argument count.
+ * @argv: argument vector
+ *
+ * Return: 0 for success.
+ */
+int main(int_attribute_((_unused_)) argc, char *argv[])
+{
+	int digit1, digit2;
+	char *ac;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	digit1 = atoi(argv[1]);
+	ac = argv[2];
+	digit2 = atoi(argv[3]);
+
+	if (get_op_func(op) == NULL || op[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((*ac == '/' && digit2 == 0) || (*ac == '%' && digit2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", get_op_func(ac)(digit1, digit2));
+	return (0);
+}
