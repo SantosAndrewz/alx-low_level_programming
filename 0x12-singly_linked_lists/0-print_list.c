0bae7022ea5ac68f69a35c1d79ada686b49eb998
SantosAndrewz <sentosandrewz@gmail.com>
2023-10-19 16:49:12 -0700

    Print list_upto date

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
+#include <string.h>
 /**
  * print_list - prints all the elements of a list_t list.
  * @h: pointer to the list's head.
@@ -12,7 +13,7 @@ size_t print_list(const list_t *h)
 {
 	size_t i;
 
-	for (i = 0; h != NULL; h = h->next)
+	for (i = 0; h != NULL; i++)
 	{
 		if (h->str == NULL)
 		{
@@ -20,9 +21,9 @@ size_t print_list(const list_t *h)
 		}
 		else
 		{
-			print("[%d] %s\n", h->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 		}
-		i++;
+		h = h->next;
 	}
 	return (i);
 }
