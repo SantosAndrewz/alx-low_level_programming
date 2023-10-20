84db8904981bb4584d4e56a97148c19892b22243
SantosAndrewz <sentosandrewz@gmail.com>
2023-10-19 18:21:26 -0700

    add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -8,7 +8,7 @@
  *
  *  Return: Length of the string.
  */
-_strglen(const char *a)
+int _strglen(const char *a)
 {
 	int x = 0;
 
@@ -29,11 +29,10 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *added_node;
 
 	added_node = malloc(sizeof(list_t));
+	if (added_node == NULL)
 	{
-		if (added_node == NULL)
-		{
-			return (NULL);
-		}
+		return (NULL);
+	}
 	added_node->str = strdup(str);
 	added_node->len = _strglen(str);
 	added_node->next = *head;
