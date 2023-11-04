6124e2c803bb5cef82b294c2e490b7c78245c0c2
SantosAndrewz <sentosandrewz@gmail.com>
2023-11-03 20:26:37 -0700

    Copy-file edited

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -20,13 +20,13 @@ void error_handle(int code, const char  *msg)
 		case 97:
 			exit(97);
 		case 98:
-                        exit(98);
+			exit(98);
 		case 99:
-                        exit(99);
+			exit(99);
 		case 100:
-                        exit(100);
+			exit(100);
 		default:
-                        exit(1);
+			exit(1);
 	}
 }
 /**
@@ -43,40 +43,39 @@ int main(int argc, char *argv[])
 	ssize_t read_txt, written_txt;
 
 	if (argc != 3)
-		error_handle(97);
+		error_handle(97, "Usage: cp file_from file_to");
 
-	from_f = open(argv[1], O_RNOLY);
+	from_f = open(argv[1], O_RDONLY);
 	if (from_f == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", from_f);
-		error_handle(98;
+		dprintf(STDERR_FILENO, "Error: Can't read from file %d\n", from_f);
+		error_handle(98, "Error: Can't read from file from_f");
 	}
 	to_f = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (to_f == -1)
-		error_handle(99);
-	
+		error_handle(99, "Error: Can't read from file to_f");
 	for (;;)
 	{
 		read_txt = read(from_f, buf, BUFFER_S);
 		if (read_txt == 0)
 			break;
 		if (read_txt == -1)
-			error_handle(98);
+			error_handle(98, "Error: Can't read from file from_f");
 		written_txt = write(to_f, buf, read_txt);
 		if (written_txt != read_txt)
-			error_handle(99);
+			error_handle(99, "Error: Can't write to file to_f");
 	}
 	close(from_f);
 	close(to_f);
 	if (close(from_f) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close file %s\n" from_f);
-		error_handle(100);
+		dprintf(STDERR_FILENO, "Error: Can't close file %d\n", from_f);
+		error_handle(100, "Error: Can't close file from_f");
 	}
 	if (close(to_f) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close file %s\n" to_f);
-                error_handle(100);
+		dprintf(STDERR_FILENO, "Error: Can't close file %d\n", to_f);
+		error_handle(100, "Error: Can't close file to_f");
 	}
-	return 0;
+	return (0);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -12,4 +12,6 @@ int create_file(const char *filename, char *text_content);
 
 int append_text_to_file(const char *filename, char *text_content);
 
+void error_handle(int code, const char  *msg);
+
 #endif
